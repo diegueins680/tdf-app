14211412db747a1a9b8e1e18d943dedcdf9d4a4e
Diego Saá <diegosaa@example.com>
2025-12-02 11:01:07 -0500

    Harden CMS payload decoding for legacy data

--- tdf-hq/src/TDF/CMS/Models.hs
+++ tdf-hq/src/TDF/CMS/Models.hs
@@ -17,10 +17,12 @@ module TDF.CMS.Models where
 import Database.Persist.TH
 import Data.Time (UTCTime)
 import Data.Text (Text)
-import Data.Aeson (Value, encode, eitherDecodeStrict')
+import Data.Aeson (Value(..), encode, eitherDecodeStrict')
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Data.Text.Encoding (decodeUtf8, decodeUtf8With, encodeUtf8)
+import Data.Text.Encoding.Error (lenientDecode)
 import Database.Persist
 import Database.Persist.Sql (PersistFieldSql(..), SqlType(..))
 import TDF.Models (PartyId)
@@ -29,18 +31,22 @@ newtype AesonValue = AesonValue { unAesonValue :: Value } deriving stock (Show, 
 
 instance PersistField AesonValue where
   toPersistValue (AesonValue v) = PersistText . decodeUtf8 . BL.toStrict $ encode v
-  fromPersistValue (PersistText t) =
-    case eitherDecodeStrict' (encodeUtf8 t) of
-      Left err -> Left (T.pack err)
-      Right val -> Right (AesonValue val)
-  fromPersistValue (PersistByteString bs) =
-    case eitherDecodeStrict' bs of
-      Left err -> Left (T.pack err)
-      Right val -> Right (AesonValue val)
+  fromPersistValue (PersistText t) = AesonValue <$> decodeValue (encodeUtf8 t)
+  fromPersistValue (PersistByteString bs) = AesonValue <$> decodeValue bs
   fromPersistValue other = Left $ "Expected JSON text, got: " <> T.pack (show other)
 
 instance PersistFieldSql AesonValue where
   sqlType _ = SqlString
+
+-- | Decode JSON stored as text/bytea, but gracefully fall back to wrapping
+-- non-JSON payloads as a JSON string so legacy rows don't crash the API.
+decodeValue :: BS.ByteString -> Either T.Text Value
+decodeValue bs =
+  case eitherDecodeStrict' bs of
+    Right val -> Right val
+    Left _ ->
+      let fallback = decodeUtf8With lenientDecode bs
+      in Right (String fallback)
 
 share [mkPersist sqlSettings, mkMigrate "migrateCMS"] [persistLowerCase|
 CmsContent
