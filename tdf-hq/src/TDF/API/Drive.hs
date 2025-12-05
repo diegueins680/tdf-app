d9d686f050a7fd29e586132a49ee93f606bad5aa
Diego Saá <diegosaa@example.com>
2025-12-05 03:10:08 -0500

    Refine Drive upload multipart parsing

--- tdf-hq/src/TDF/API/Drive.hs
+++ tdf-hq/src/TDF/API/Drive.hs
@@ -9,7 +9,7 @@ module TDF.API.Drive where
 import           Data.Text          (Text)
 import           GHC.Generics       (Generic)
 import           Servant
-import           Servant.Multipart  (MultipartForm, Tmp, FromMultipart(..), lookupFile, lookupInput, FileData)
+import           Servant.Multipart  (MultipartForm, Tmp, FromMultipart(..), lookupFile, lookupInput, FileData, Input(..))
 import qualified Data.Text         as T
 
 import           TDF.API.Types      (DriveUploadDTO)
@@ -24,9 +24,15 @@ data DriveUploadForm = DriveUploadForm
 instance FromMultipart Tmp DriveUploadForm where
   fromMultipart multipart = do
     file <- lookupFile "file" multipart
-    let folder = either (const Nothing) (Just . T.strip) (lookupInput "folderId" multipart)
-        nameTxt = either (const Nothing) (Just . T.strip) (lookupInput "name" multipart)
-        token = either (const Nothing) (Just . T.strip) (lookupInput "accessToken" multipart)
+    let folder = case lookupInput "folderId" multipart of
+          Right Input{ iValue = v } -> Just (T.strip v)
+          _                         -> Nothing
+        nameTxt = case lookupInput "name" multipart of
+          Right Input{ iValue = v } -> Just (T.strip v)
+          _                         -> Nothing
+        token = case lookupInput "accessToken" multipart of
+          Right Input{ iValue = v } -> Just (T.strip v)
+          _                         -> Nothing
     pure DriveUploadForm
       { duFile = file
       , duFolderId = folder
